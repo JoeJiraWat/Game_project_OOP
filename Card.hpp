82d91d87a78e987286b00ecee543606b8c547a6c
LikeSushi <88445938+LikeSushi@users.noreply.github.com>
2026-05-09 15:11:51 +0700

    pushhhhhhhhhhhhhhhhhhh

--- Card.hpp
+++ Card.hpp
@@ -16,7 +16,58 @@ class card{
         card(std::string t);
         std::string getType();
 };
-class skip: public card{
+class Boom: public card{
+    private:
+        Boom(std::string t);
+    public://รอเพื่อนเขียน game manage
+        void Onuse();
+};
+class Default: public card{
+    private:
+        Default(std::string t);
     public:
-        skip(std::string t);
+        void Onuse();
+};
+class Skip: public card{
+    private:
+        Skip(std::string t);
+    public:
+        void Onuse();
+}
+class Favor: public card{
+    private:
+        Favor(std::string t);
+    public:
+        void Onuse();
+};
+class See_the_future: public card{
+    private:
+        See_the_future(std::string t);
+    public:
+        void Onuse();
+};
+class Shuffle: public card{
+    private:
+        Shuffle(std::string t);
+    public:
+        void Onuse();
+};
+class Nope: public card{
+    private:
+        Nope(std::string t);
+    public:
+        void Onuse();
+};
+class Draw_from_the_bottom: public card{
+    private:
+        Draw_from_the_bottom(std::string t);
+    public:
+        void Onuse();
+};
+class Cat_card: public card{
+    private:
+        int quantity;
+        enum cat_card_type{Tacocat,Hairy_Potato_Cat,Beard_cat,Rainbow_Ralphing_Cat,Cattermelon} type;
+    public:
+        void Onuse();
 };
